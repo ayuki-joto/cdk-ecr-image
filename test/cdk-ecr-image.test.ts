42ed9ef276f647cfe95d42c573d38f31df9c8670
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-06-03 23:38:49 +0900

    Initial commit

---
+++ test/cdk-ecr-image.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEcrImage from '../lib/cdk-ecr-image-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ecr-image-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEcrImage.CdkEcrImageStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
